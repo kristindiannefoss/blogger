ba062be6a498098e7ccec9b2fed3fe2331fb4176
Kris Foss <kris.foss@gmail.com>
2016-04-06 23:07:57 -0600

    completes i3

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tag = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to tags_path
+  end
+  
+end
---
+++ app/models/tag.rb
@@ -0,0 +1,14 @@
+class Tag < ActiveRecord::Base
+  has_many :taggings
+  has_many :articles, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def to_s
+  name
+  end
+end
